0c7cb9517bee5d408be4506c7cff30022f0d204b
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-01-06 21:25:10 -0500

    show toDos on the page as li elementts inside ul element

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if (toDo === "") return;
     setToDo("");
-  }
+    setToDos((prev) => [toDo, ...prev]);
+  };
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -19,6 +22,12 @@ function App() {
         />
         <button>Add ToDo</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, idx) => (
+          <li key={idx}>{toDo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
