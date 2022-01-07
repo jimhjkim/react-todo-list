41391cca68400bcad0f358b846d87adb13e58169
Jim Kim <jim.hyunjin.kim@gmail.com>
2022-01-06 20:49:42 -0500

    Add form to write todos and submit via button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDo("");
+  }
   return (
     <div>
-      Hello World
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your todo..."
+        />
+        <button>Add ToDo</button>
+      </form>
     </div>
   );
 }
